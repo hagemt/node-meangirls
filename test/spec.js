3b594ba3e4c97463eeb0c85413a552f7c0d892ff
Tor Hagemann <tohagema@cisco.com>
2016-12-25 20:52:09 +0000

    package.json: add vacuous mocha (npm test)

---
+++ test/spec.js
@@ -0,0 +1,11 @@
+/* eslint-env es6, mocha, node */
+const assert = require('assert');
+const meangirls = require('..');
+
+describe('meangirls', () => {
+
+	it('is a library for CRDTs', () => {
+		assert(meangirls);
+	});
+
+});
