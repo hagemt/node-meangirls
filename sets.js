f73be572f3376e8b3f4194199fa2d12511364377
Tor Hagemann <tohagema@cisco.com>
2017-01-01 01:43:55 +0000

    sets: implement a few more-useful methods

--- sets.js
+++ sets.js
@@ -4,87 +4,103 @@ const { EventEmitter } = require('events');
 const SETS = new WeakMap(); // private
 
 // TODO (tohagema): refactor into class modules?
-// TODO (tohagema): refactor #merge to be static?
+// TODO (tohagema): implement get size uniformly?
+
+const toJSON = (object = null, ...args) => {
+	if (typeof object !== 'object') return object;
+	return JSON.stringify(object, ...args);
+};
 
 class GSet extends EventEmitter {
 
-	constructor () {
+	constructor (...args) {
 		super(); // mistake to inherit?
-		SETS.set(this, Object.freeze({ e: new Set() }));
+		SETS.set(this, Object.freeze({ e: new Set(...args) }));
 	}
 
-	merge (other) {
-		if (!(other instanceof GSet)) {
-			throw new TypeError('expected GSet');
-		}
-		const merged = new GSet();
-		const { e: e0 } = SETS.get(merged);
-		const { e: e1 } = SETS.get(other);
-		const { e: e2 } = SETS.get(this);
-		for (const e of e1) e0.add(e);
-		for (const e of e2) e0.add(e);
-		return merged;
+	contains (element) {
+		const { e } = SETS.get(this);
+		return e.has(element);
 	}
 
 	insert (element) {
 		const { e } = SETS.get(this);
-		e.add(element); // => true
+		e.add(element); // returns Set
 		this.emit('element', element);
 		return this;
 	}
 
-	* [Symbol.iterator] () {
+	get size () {
 		const { e } = SETS.get(this);
-		for (const ee of e) yield ee;
+		return e.size;
 	}
 
-	static fromJSON ({ e = [] }, ...args) {
+	* [Symbol.iterator] () {
+		const { e } = SETS.get(this);
+		yield* e[Symbol.iterator]();
+	}
+
+	static fromJSON ({ e }, ...args) {
 		const set = new GSet();
 		const { e: _e } = SETS.get(set);
 		for (const element of e) _e.add(JSON.parse(element, ...args));
 		return set;
 	}
 
+	static merge (first, second) {
+		if (!(first instanceof GSet) || !(second instanceof GSet)) {
+			throw new TypeError('expected GSet');
+		}
+		const self = new GSet();
+		const { e: e0 } = SETS.get(self);
+		const { e: e1 } = SETS.get(first);
+		const { e: e2 } = SETS.get(second);
+		for (const e of e1) e0.add(e);
+		for (const e of e2) e0.add(e);
+		return self;
+	}
+
 	static toJSON (anyGSet, ...args) {
 		const { e } = SETS.get(anyGSet);
-		const toJSON = element => JSON.stringify(element, ...args);
-		return { e: Array.from(e, toJSON), type: 'g-set' };
+		const elements = Array.from(e, element => toJSON(element, ...args));
+		return { e: elements, type: 'g-set' };
 	}
 
 }
 
 class TwoPSet extends EventEmitter {
 
-	constructor () {
+	constructor (...args) {
 		super(); // mistake to inherit?
-		SETS.set(this, Object.freeze({ a: new Set(), r: new Set() }));
+		SETS.set(this, Object.freeze({ a: new Set(...args), r: new Set() }));
 	}
 
-	merge (other) {
-		if (!(other instanceof TwoPSet)) {
-			throw new TypeError('expected TwoPSet');
-		}
-		const merged = new TwoPSet();
-		const { a: a0 } = SETS.get(merged);
-		const { a: a1 } = SETS.get(other);
-		const { a: a2 } = SETS.get(this);
-		for (const e in a1) a0.add(e);
-		for (const e in a2) a0.add(e);
-		return merged;
+	contains (element) {
+		const { a, r } = SETS.get(this);
+		return a.has(element) && !r.has(element);
 	}
 
 	insert (element) {
 		const { a } = SETS.get(this);
-		a.add(element); // right?
-		this.emit('element', element);
+		if (!a.has(element)) {
+			this.emit('insert', element);
+			a.add(element);
+		}
 		return this;
 	}
 
 	remove (element) {
-		const { r } = SETS.get(this);
-		r.add(element); // right?
-		this.emit('remove', element);
+		const { a, r } = SETS.get(this);
+		if (a.has(element) && !r.has(element)) {
+			this.emit('remove', element);
+			r.add(element);
+		}
 		return this;
+	}
+
+	get size () {
+		const { a, r } = SETS.get(this);
+		return a.size - r.size;
 	}
 
 	* [Symbol.iterator] () {
@@ -94,7 +110,7 @@ class TwoPSet extends EventEmitter {
 		}
 	}
 
-	static fromJSON ({ a = [], r = [] }, ...args) {
+	static fromJSON ({ a, r }, ...args) {
 		const set = new TwoPSet();
 		const { a: _a, r: _r } = SETS.get(set);
 		for (const e of a) _a.add(JSON.parse(e, ...args));
@@ -102,10 +118,26 @@ class TwoPSet extends EventEmitter {
 		return set;
 	}
 
+	static merge (first, second) {
+		if (!(first instanceof TwoPSet) || !(second instanceof TwoPSet)) {
+			throw new TypeError('expected TwoPSet');
+		}
+		const self = new TwoPSet();
+		const { a: a0, r: r0 } = SETS.get(self);
+		const { a: a1, r: r1 } = SETS.get(first);
+		const { a: a2, r: r2 } = SETS.get(second);
+		for (const e of a1) a0.add(e);
+		for (const e of a2) a0.add(e);
+		for (const e of r1) r0.add(e);
+		for (const e of r2) r0.add(e);
+		return self;
+	}
+
 	static toJSON (anyTwoPSet, ...args) {
 		const { a, r } = SETS.get(anyTwoPSet);
-		const toJSON = set => Array.from(set, e => JSON.stringify(e, ...args));
-		return { a: toJSON(a), r: toJSON(r), type: '2p-set' };
+		const added = Array.from(a, e => toJSON(e, ...args));
+		const removed = Array.from(r, e => toJSON(e, ...args));
+		return { a: added, r: removed, type: '2p-set' };
 	}
 
 }
@@ -115,34 +147,32 @@ class LWWESet extends EventEmitter {
 	constructor (bias = 'r') {
 		super(); // mistake to inherit?
 		// TODO (tohagema): support 'a' bias (and make default)
-		if (bias !== 'r') throw new TypeError('bias must be "r", for now');
+		if (bias !== 'r') throw new Error('bias must be "r", for now');
 		SETS.set(this, Object.freeze({ a: new Map(), bias, r: new Map() }));
 	}
 
-	merge (other) {
-		if (!(other instanceof LWWESet)) {
-			throw new TypeError('expected LWWESet');
-		}
-		const merged = new LWWESet();
-		const { a: a0, r: r0 } = SETS.get(merged);
-		const { a: a1, r: r1 } = SETS.get(other);
-		const { a: a2, r: r2 } = SETS.get(this);
-		for (const e in a1) a0.add(e);
-		for (const e in a2) a0.add(e);
-		for (const e in r1) r0.add(e);
-		for (const e in r2) r0.add(e);
-		return merged;
+	contains (key) {
+		const { a, r } = SETS.get(this);
+		if (!a.has(key)) return false;
+		if (!r.has(key)) return true;
+		return r.get(key) < a.get(key);
 	}
 
-	insert (e, t = new Date()) {
-		const { a } = SETS.get(this);
-		a.set(e, t);
+	insert (key, value = new Date()) {
+		const { a, r } = SETS.get(this);
+		if (!a.has(key)) {
+			this.emit('insert', [key, value], [a.get(key), r.get(key)]);
+		}
+		a.set(key, value);
 		return this;
 	}
 
-	remove (e, t = new Date()) {
-		const { r } = SETS.get(this);
-		r.set(e, t);
+	remove (key, value = new Date()) {
+		const { a, r } = SETS.get(this);
+		if (a.has(key)) {
+			this.emit('remove', [key, value], [a.get(key), r.get(key)]);
+			r.set(key, value);
+		}
 		return this;
 	}
 
@@ -153,28 +183,59 @@ class LWWESet extends EventEmitter {
 		}
 	}
 
-	static fromJSON ({ bias, e = [] }, ...args) {
+	static fromJSON ({ bias, e }, ...args) {
+		const start = new Date();
 		const set = new LWWESet(bias);
 		const { a, r } = SETS.get(set);
 		for (const tuple of e) {
 			const [anyJSON, aDate, rDate] = tuple;
 			const element = JSON.parse(anyJSON, ...args);
-			a.set(element, new Date(aDate));
+			if (tuple.length === 1) {
+				a.set(element, start);
+			}
+			if (tuple.length === 2) {
+				a.set(element, new Date(aDate));
+			}
 			if (tuple.length === 3) {
+				a.set(element, new Date(aDate));
 				r.set(element, new Date(rDate));
 			}
 		}
 		return set;
 	}
 
+	static merge (first, second) {
+		if (!(first instanceof LWWESet) || !(second instanceof LWWESet)) {
+			throw new TypeError('expected LWWESet');
+		}
+		const self = new LWWESet();
+		const { a: a0, r: r0 } = SETS.get(self);
+		const { a: a1, r: r1 } = SETS.get(first);
+		const { a: a2, r: r2 } = SETS.get(second);
+		for (const [k, v] in a1) {
+			if (!a0.has(k) || a0.get(k) < v) a0.put(k, v);
+		}
+		for (const [k, v] in a2) {
+			if (!a0.has(k) || a0.get(k) < v) a0.put(k, v);
+		}
+		for (const [k, v] in r1) {
+			if (!r0.has(k) || r0.get(k) < v) r0.put(k, v);
+		}
+		for (const [k, v] in r2) {
+			if (!r0.has(k) || r0.get(k) < v) r0.put(k, v);
+		}
+		return self;
+	}
+
 	static toJSON (anyLWWESet, ...args) {
 		const { a, bias, r } = SETS.get(anyLWWESet);
-		const toJSON = ([k, v]) => {
-			const element = [JSON.stringify(k, ...args), +v];
+		const elements = []; // "e"
+		for (const [k, v] of a) {
+			const element = [toJSON(k, ...args), +v];
 			if (r.has(k)) element.push(+r.get(k));
-			return element;
+			elements.push(element);
 		}
-		return { bias, e: Array.from(a, toJSON), type: 'lww-e-set' };
+		return { bias, e: elements, type: 'lww-e-set' };
 	}
 
 }
@@ -200,36 +261,28 @@ const memoize = (f, resolver = (first => first)) => {
 	return Object.defineProperties(g, {
 		cache: { value: cache },
 		name: { value: f.name },
-		raw: { value: f },
 	});
 };
 
-const getTagFunction = () => {
+const tagFunction = () => {
 	const tags = Object.assign(memoize(Symbol, String), { next: 0 });
 	return value => isString(value) ? tags(value) : tags(tags.next++);
 };
 
 class ORSet extends EventEmitter {
 
-	constructor (tag = getTagFunction()) {
+	constructor (tag = tagFunction()) {
 		super(); // mistake to inherit?
 		SETS.set(this, Object.freeze({ a: new Map(), r: new Map(), tag }));
 		Object.freeze(this);
 	}
 
-	merge (other) {
-		if (!(other instanceof ORSet)) {
-			throw new TypeError('expected ORSet');
-		}
-		const merged = new ORSet();
-		const { a: a0, r: r0 } = SETS.get(merged);
-		const { a: a1, r: r1 } = SETS.get(other);
-		const { a: a2, r: r2 } = SETS.get(this);
-		for (const [k, v] of a1) a0.set(k, getMapSet(k, a0).add(v));
-		for (const [k, v] of a2) a0.set(k, getMapSet(k, a0).add(v));
-		for (const [k, v] of r1) r0.set(k, getMapSet(k, r0).add(v));
-		for (const [k, v] of r2) r0.set(k, getMapSet(k, r0).add(v));
-		return merged;
+	contains (key) {
+		const { a, r } = SETS.get(this);
+		if (!a.has(key)) return false;
+		if (!r.has(key)) return true;
+		const [aSet, rSet] = [a.get(key), r.get(key)];
+		return aSet.some(tag => !rSet.has(tag));
 	}
 
 	insert (key, value) {
@@ -248,13 +301,13 @@ class ORSet extends EventEmitter {
 
 	* [Symbol.iterator] () {
 		const { a, r } = SETS.get(this);
-		for (const [k, v] of a) {
-			const tags = v.filter(e => r.has(e) ? !r.get(e).has(v) : true);
-			if (tags.length > 0) yield k;
+		for (const [element, aSet] of a) {
+			const rSet = tag => r.has(element) ? !r.get(element).has(tag) : true;
+			if (aSet.filter(rSet).length > 0) yield element;
 		}
 	}
 
-	static fromJSON ({ e = [] }, ...args) {
+	static fromJSON ({ e }, ...args) {
 		const set = new ORSet();
 		const { a, r, tag } = SETS.get(set);
 		for (const [ek, ea, er] of e) {
@@ -265,15 +318,30 @@ class ORSet extends EventEmitter {
 		return set;
 	}
 
+	static merge (first, second) {
+		if (!(first instanceof ORSet) || !(second instanceof ORSet)) {
+			throw new TypeError('expected ORSet');
+		}
+		const self = new ORSet();
+		const { a: a0, r: r0 } = SETS.get(self);
+		const { a: a1, r: r1 } = SETS.get(first);
+		const { a: a2, r: r2 } = SETS.get(second);
+		for (const [k, v] of a1) a0.set(k, getMapSet(k, a0).add(v));
+		for (const [k, v] of a2) a0.set(k, getMapSet(k, a0).add(v));
+		for (const [k, v] of r1) r0.set(k, getMapSet(k, r0).add(v));
+		for (const [k, v] of r2) r0.set(k, getMapSet(k, r0).add(v));
+		return self;
+	}
+
 	static toJSON (anyORSet, ...args) {
 		const { a, r } = SETS.get(anyORSet);
-		const e = []; // elements
+		const elements = []; // "e"
 		for (const [k, v] of a) {
-			const element = [JSON.stringify(k, ...args), Array.from(v, toNumber)];
+			const element = [toJSON(k, ...args), Array.from(v, toNumber)];
 			if (r.has(k)) element.push(Array.from(r.get(k), toNumber));
-			e.push(element);
+			elements.push(element);
 		}
-		return { e, type: 'or-set' };
+		return { e: elements, type: 'or-set' };
 	}
 
 }
@@ -285,65 +353,77 @@ class MCSet extends EventEmitter {
 		SETS.set(this, Object.freeze({ e: new Map() }));
 	}
 
-	merge (other) {
-		if (!(other instanceof MCSet)) {
-			throw new TypeError('expected MCSet');
-		}
-		const merged = new MCSet();
-		const { e: e0 } = SETS.get(merged);
-		const { e: e1 } = SETS.get(other);
-		const { e: e2 } = SETS.get(this);
-		for (const [k, v] in e1) e0.set(k, v + (e0.get(k) || 0));
-		for (const [k, v] in e2) e0.set(k, v + (e0.get(k) || 0));
-		return merged;
+	contains (element) {
+		const { e } = SETS.get(this);
+		return e.has(element) && (e.get(element) % 2 === 1);
 	}
 
-	insert (key) {
+	insert (element) {
 		const { e } = SETS.get(this);
-		const value = e.get(key) || 0;
-		if (value % 2 === 0) e.set(key, 1 + value);
+		const number = e.get(element) || 0;
+		if (number % 2 === 0) e.set(element, 1 + number);
 		return this;
 	}
 
-	remove (key) {
+	remove (element) {
 		const { e } = SETS.get(this);
-		const value = e.get(key) || 0;
-		if (value % 2 === 1) e.set(key, 1 + value);
+		const number = e.get(element) || 0;
+		if (number % 2 === 1) e.set(element, 1 + number);
 		return this;
 	}
 
 	* [Symbol.iterator] () {
 		const { e } = SETS.get(this);
-		for (const [k, v] of e) {
-			if (v % 2 === 1) yield k;
+		for (const [element, number] of e) {
+			if (number % 2 === 1) yield element;
 		}
 	}
 
-	static fromJSON ({ e = [] }, ...args) {
+	static fromJSON ({ e }, ...args) {
 		const set = new MCSet();
 		const { e: e0 } = SETS.get(set);
 		for (const [element, n] of e) {
-			e0.set(JSON.parse(element, ...args), Number(n));
+			if (!(Number.isInteger(n) && n > 0)) {
+				throw new Error('expected positive Integer');
+			}
+			e0.set(JSON.parse(element, ...args), n);
 		}
 		return set;
 	}
 
+	static merge (first, second) {
+		if (!(first instanceof MCSet) || !(second instanceof MCSet)) {
+			throw new TypeError('expected MCSet');
+		}
+		const self = new MCSet();
+		const { e: e0 } = SETS.get(self);
+		const { e: e1 } = SETS.get(first);
+		const { e: e2 } = SETS.get(second);
+		for (const [element, n] in e1) {
+			e0.set(element, n + (e0.get(element) || 0));
+		}
+		for (const [element, n] in e2) {
+			e0.set(element, n + (e0.get(element) || 0));
+		}
+		return self;
+	}
+
 	static toJSON (anyMCSet, ...args) {
 		const { e } = SETS.get(anyMCSet);
-		const toJSON = (k, v) => [JSON.stringify(k, ...args), v];
-		return { e: Array.from(e, ([k, v]) => toJSON(k, v)), type: 'mc-set' };
+		const elements = Array.from(e, ([k, v]) => [toJSON(k, ...args), v]);
+		return { e: elements, type: 'mc-set' };
 	}
 
 }
 
-const merge2 = (one, two) => one.merge(two); // "static"
-Object.defineProperty(merge2, 'name', { value: 'merge' });
-
+/* istanbul ignore next */
 for (const T of [GSet, TwoPSet, LWWESet, ORSet, MCSet]) {
 	T.prototype.inspect = function inspect () {
 		return T.toJSON(this);
 	};
-	Object.defineProperty(T, 'merge', { value: merge2 });
+	T.prototype.merge = function merge (other) {
+		return T.merge(this, other);
+	};
 }
 
 module.exports = { GSet, LWWESet, MCSet, ORSet, TwoPSet };
